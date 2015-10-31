889d7955ea873d6942e24f279952a8e9965f0669
Tejesh <tejesh22.raut@gmail.com>
2015-10-31 11:47:07 +0530

    deleted previous mysite1 folder

--- mysite1/bc/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import RollNo
-
-class RollForm(forms.ModelForm):
-
-    class Meta:
-        model = RollNo
-        fields = ('rollno_text', 'name_text','cpi','category' , 'current_branch','choice_1','choice_2','choice_3','choice_4')
